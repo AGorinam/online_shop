00a4f72cd3e8f3f00fa893047587122e792a8a7c
Alejandro Gorina <alejandrogorinam@gmail.com>
2022-01-25 16:55:34 +0100

    FIX: Corrected worng foreign keys order_items and orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_25_152659) do
+ActiveRecord::Schema.define(version: 2022_01_25_155344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2022_01_25_152659) do
     t.decimal "price", precision: 15, scale: 2, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "order_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -34,8 +36,6 @@ ActiveRecord::Schema.define(version: 2022_01_25_152659) do
     t.decimal "sub_total", precision: 15, scale: 2, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "order_id", null: false
-    t.index ["order_id"], name: "index_orders_on_order_id"
   end
 
   create_table "product_categories", force: :cascade do |t|
@@ -76,7 +76,7 @@ ActiveRecord::Schema.define(version: 2022_01_25_152659) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "orders", "orders"
+  add_foreign_key "order_items", "orders"
   add_foreign_key "product_categories", "categories"
   add_foreign_key "product_categories", "products"
   add_foreign_key "product_variants", "products"
